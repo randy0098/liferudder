8e34e12c1bf58da3b1e1d258b058e4ebf377d7b8
randy.xia98@gmail.com <randy.xia98@gmail.com@e651ba81-2ec6-5316-895e-154824fd0fbb>
2012-08-25 03:26:48 +0000

    use Spring's HibernateDaoSupport to implement BaseDAO.

--- branches/SSH/src/main/java/framework/BaseDAO.java
+++ branches/SSH/src/main/java/framework/BaseDAO.java
@@ -7,27 +7,8 @@
 
 package framework;
 
-import org.hibernate.SessionFactory;
-import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public class BaseDAO {
-	private SessionFactory sessionFactory;
-
-	private HibernateTemplate hibernateTemplate;
-
-	public SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
-
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
-
-	public HibernateTemplate getHibernateTemplate() {
-		if (hibernateTemplate == null) {
-			hibernateTemplate = new HibernateTemplate(sessionFactory);
-		}
-		return hibernateTemplate;
-	}
+public class BaseDAO extends HibernateDaoSupport{
 
 }

96b4186a8dac27a753094c1bc4725ecf305829e6
randy.xia98@gmail.com <randy.xia98@gmail.com@e651ba81-2ec6-5316-895e-154824fd0fbb>
2012-09-05 06:50:09 +0000

    design bo layer and add transaction control to bo classes with BeanNameAutoProxyCreator instead of DefaultAdvisorAutoProxyCreator.

--- src/main/java/dao/MessageDAO.java
+++ src/main/java/dao/MessageDAO.java
@@ -22,6 +22,6 @@ public interface MessageDAO extends BaseDAO{
 	
 	public void updateMessage(MessageVO message);
 	
-	public void deleteMessage(String id);
+	public void deleteMessage(int id);
 	
 }

1ac17f2efbaa73793ba9a70df1a39d4f5547391e
randy.xia98@gmail.com <randy.xia98@gmail.com@e651ba81-2ec6-5316-895e-154824fd0fbb>
2013-06-14 12:38:07 +0000

    test authority interceptor.

---
+++ branches/SSH2/src/main/java/framework/AuthorityInterceptor.java
@@ -0,0 +1,25 @@
+package framework;
+
+import java.util.Map;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.MethodFilterInterceptor;
+
+public class AuthorityInterceptor extends MethodFilterInterceptor{
+	@Override
+	protected String doIntercept(ActionInvocation invocation)
+			throws Exception {
+		ActionContext context = invocation.getInvocationContext();
+		Map session	= context.getSession();
+		String user = (String) session.get("user");
+		System.out.println("user:"+user);
+		if(user!=null && user.equalsIgnoreCase("hp")){
+			return invocation.invoke();
+		}
+		context.put("tip", "您还没登陆");
+		return Action.LOGIN;
+	}
+
+}

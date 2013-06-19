9075a36b38b04f6cde8ab96c364a319407fe8995
randy.xia98@gmail.com <randy.xia98@gmail.com@e651ba81-2ec6-5316-895e-154824fd0fbb>
2013-06-19 12:09:59 +0000

    test ajax 1.

--- branches/SSH2/src/main/java/action/LoginAjaxAction.java
+++ branches/SSH2/src/main/java/action/LoginAjaxAction.java
@@ -3,12 +3,9 @@ package action;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import javassist.bytecode.ByteArray;
-
-import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class LoginAction extends ActionSupport {
+public class LoginAjaxAction extends ActionSupport {
 	private String username;
 	private String password;
 	private InputStream inputStream;
@@ -31,22 +28,6 @@ public class LoginAction extends ActionSupport {
 
 	public void setPassword(String password) {
 		this.password = password;
-	}
-
-	public String login() throws Exception {
-		System.out.println("login----------------------");
-		if (getUsername().equals("hp")
-				&& getPassword().equals("randy")) {
-			ActionContext.getContext().getSession().put("user", getUsername());
-			return SUCCESS;
-		} else {
-			return ERROR;
-		}
-	}
-	
-	public String query() throws Exception {
-		System.out.println("query----------------------");
-		return "query";
 	}
 	
 	public String ajaxLoginTest() throws Exception {
